ed4be6ffc6a3b4d0e42eddfca2bc23e3c9ed6db9
Håkon S.U <haakonu97@gmail.com>
2021-01-31 12:16:08 +0100

    Added new build

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,67 @@ type Page = "home" | "contact" | "portfolio";
 function App() {
   const [page, setPage] = useState<Page>("home");
   const [canAnimate, setCanAnimate] = useState<Page[]>(["home"]);
+  const [formState, setFormState] = useState({
+    name: "",
+    reply_to: "",
+    message: "",
+  });
+  const [loadingForm, setLoadingForm] = useState(false);
+  const [formSent, setFormSent] = useState(false);
+  const [fillFields, setFillFields] = useState(false);
+
+  const setState = (key: string, value: string) => {
+    setFormState({ ...formState, [key]: value });
+    setFormSent(false);
+    setFillFields(false);
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (formState.name && formState.message && formState.reply_to) {
+      console.log("fetching...");
+      setLoadingForm(true);
+      fetch(
+        "https://ewxpkphj05.execute-api.us-east-1.amazonaws.com/dev/static-site-mailer",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formState),
+        }
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          console.log("Successful response from server: ", res);
+          setLoadingForm(false);
+          setFormState({
+            name: "",
+            reply_to: "",
+            message: "",
+          });
+          setFormSent(true);
+        })
+        .catch((err) => {
+          console.error("Unsuccessful response from server: ", err);
+          setLoadingForm(false);
+          setFormState({
+            name: "",
+            reply_to: "",
+            message: "",
+          });
+          setFormSent(true);
+        });
+    } else {
+      //
+      setFillFields(true);
+    }
+    return;
+  };
 
   useEffect(() => {
     if (!canAnimate.includes(page)) {
       setCanAnimate([...canAnimate, page]);
     }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
   const homeCN = CN(
@@ -53,21 +109,43 @@ function App() {
         </div>
         <div className={contactCN}>
           <h2>KONTAKT</h2>
+          {formSent && (
+            <span className="succ">
+              <strong> Meldingen ble sendt. </strong>Vi kontakter deg innen 2-4
+              virkedager.
+            </span>
+          )}
+          {fillFields && (
+            <span className="succ">Vennligst fyll ut alle felt.</span>
+          )}
           <label>
             Ditt navn
-            <input type="text" />
+            <input
+              type="text"
+              value={formState.name}
+              onChange={(e) => setState("name", e.target.value)}
+            />
           </label>
           <label>
             E-post adresse
-            <input type="email" />
+            <input
+              type="email"
+              value={formState.reply_to}
+              onChange={(e) => setState("reply_to", e.target.value)}
+            />
           </label>
           <label>
             Melding
-            <textarea />
+            <textarea
+              value={formState.message}
+              onChange={(e) => setState("message", e.target.value)}
+            />
           </label>
           <footer>
             <button onClick={() => setPage("home")}>Tilbake</button>
-            <button>Kontakt oss</button>
+            <button onClick={onSubmit} disabled={loadingForm}>
+              {loadingForm ? "Sender..." : "Kontakt oss"}
+            </button>
           </footer>
         </div>
         <div className={portfolioCN}>
@@ -95,11 +173,16 @@ const Container = styled.div`
   display: grid;
   grid-template-rows: 1;
   grid-template-columns: 1;
+  max-width: 85vw;
+  margin: 0 auto;
   > * {
     grid-area: 1 / 1;
   }
   h1 {
     font-size: 6em;
+    @media screen and (max-width: 800px) {
+      font-size: 4em;
+    }
     margin: 0;
     span {
       color: var(--pink-g);
@@ -108,12 +191,23 @@ const Container = styled.div`
   }
   h2 {
     opacity: 0.6;
+    @media screen and (max-width: 800px) {
+      font-size: 1.2em;
+    }
+  }
+  p {
+    @media screen and (max-width: 800px) {
+      font-size: 0.8em;
+    }
   }
   button {
     border: none;
     padding: 0.7em 1.3em;
     font-weight: bold;
     font-size: 1.3em;
+    @media screen and (max-width: 800px) {
+      font-size: 1.1em;
+    }
     cursor: pointer;
     color: var(--violet);
     background: var(--pink-g);
@@ -146,10 +240,22 @@ const Container = styled.div`
     }
     &.home button {
       margin-top: 30px;
+      @media screen and (max-width: 800px) {
+        display: block;
+        width: 100%;
+        margin: 0;
+        margin-top: 10px;
+        &:first-of-type {
+          margin-top: 25px;
+        }
+      }
     }
     &.portfolio h2,
     &.contact h2 {
       font-size: 4.5em;
+      @media screen and (max-width: 800px) {
+        font-size: 3em;
+      }
     }
     &.portfolio {
       h2 {
@@ -162,6 +268,9 @@ const Container = styled.div`
         padding-left: 20px;
         li {
           font-size: 1.2em;
+          @media screen and (max-width: 800px) {
+            font-size: 0.8em;
+          }
           a {
             color: var(--violet);
             &:hover {
@@ -175,8 +284,16 @@ const Container = styled.div`
       display: flex;
       flex-direction: column;
       width: 500px;
+      .succ {
+        display: block;
+        margin-bottom: 20px;
+        color: var(--violet);
+        background: var(--pink-l);
+        padding: 10px;
+        font-size: 0.8em;
+      }
       @media screen and (max-width: 550px) {
-        width: 95vw;
+        width: 85vw;
         padding: 0 5vmin;
       }
       h2 {
@@ -204,6 +321,17 @@ const Container = styled.div`
       }
       footer {
         margin-top: 30px;
+        @media screen and (max-width: 800px) {
+          display: flex;
+          flex-direction: column;
+          button {
+            width: 100%;
+            + button {
+              margin: 0;
+              margin-top: 10px;
+            }
+          }
+        }
       }
       &.active {
         animation: fadeInContact 0.3s ease-in;
@@ -250,6 +378,9 @@ const Mountain = styled.img<{ page: Page }>`
   bottom: 0;
   right: 2vmin;
   max-width: 45%;
+  @media screen and (max-width: 800px) {
+    max-width: 70%;
+  }
   transform-origin: bottom right;
   pointer-events: none;
   user-select: none;
@@ -276,6 +407,9 @@ const Mountain2 = styled.img<{ page: Page }>`
   bottom: 0;
   left: 0;
   max-width: 70%;
+  @media screen and (max-width: 800px) {
+    max-width: 100%;
+  }
   pointer-events: none;
   user-select: none;
   transition: transform 0.8s ease-out, opacity 0.8s ease-out;
